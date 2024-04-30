ae06d8392853f54c2b4ccb93142bde5156ff09e8
ShrinilDhorda <79510799+ShrinilDhorda@users.noreply.github.com>
2024-04-30 14:07:51 +0530

    Major changes

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
+from flask_pymongo import PyMongo
 
-
-app = Flask(__name__, template_folder='template', static_folder = 'static')
+app = Flask(__name__, template_folder='template', static_folder='static')
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/MajorWD'
+mongo = PyMongo(app)
 
 @app.route('/')
 def home():
@@ -15,13 +17,51 @@ def signup():
 def login():
     return render_template('login.html')
 
-@app.route('/police')
-def police():
-    return render_template('police.html')
+@app.route('/signup/police', methods=['POST'])
+def signup_police():
+    if request.method == 'POST':
+        branch_name = request.form.get('branchName')
+        police_email = request.form.get('policeEmail')
+        contact_number = request.form.get('contactNumber')
+        address = request.form.get('address')
+        username = request.form.get('policeUsername')
+        password = request.form.get('policePassword')
 
-@app.route('/business')
-def business():
-    return render_template('business.html')
+        police_collection = mongo.db.police
+        police_collection.insert_one({
+            'branch_name': branch_name,
+            'police_email': police_email,
+            'contact_number': contact_number,
+            'address': address,
+            'username': username,
+            'password': password
+        })
+        return 'Police signup successful'
+    else:
+        return 'Invalid request method'
 
-if (__name__ == '__main__'):
+@app.route('/signup/business', methods=['POST'])
+def signup_business():
+    if request.method == 'POST':
+        business_name = request.form.get('businessName')
+        business_email = request.form.get('businessEmail')
+        contact_number = request.form.get('businessContactNumber')
+        nearest_police_station = request.form.get('nearestPoliceStation')
+        username = request.form.get('businessUsername')
+        password = request.form.get('businessPassword')
+
+        business_collection = mongo.db.business
+        business_collection.insert_one({
+            'business_name': business_name,
+            'business_email': business_email,
+            'contact_number': contact_number,
+            'nearest_police_station': nearest_police_station,
+            'username': username,
+            'password': password
+        })
+        return 'Business signup successful'
+    else:
+        return 'Invalid request method'
+
+if __name__ == '__main__':
     app.run(debug=True)
